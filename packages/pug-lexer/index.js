5731f88a19802afd383990001dfa4f5956bd5437
jeromew <jerome.wagner@m4x.org>
2018-06-08 15:27:11 +0000

    remove mention of acorn

--- packages/pug-lexer/index.js
+++ packages/pug-lexer/index.js
@@ -74,8 +74,7 @@ Lexer.prototype = {
       return this.callLexerFunction('parseExpression', exp);
     } catch (ex) {
       if (noThrow) return false;
-
-      // not coming from acorn
+      
       if (!ex.loc) throw ex;
 
       this.incrementLine(ex.loc.line - 1);
